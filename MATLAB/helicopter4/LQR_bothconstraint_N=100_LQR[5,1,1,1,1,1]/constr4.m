d1cc4efbc0d52ea831936d3e09de5d967266ea12
Sigvart Midtsand Hovland <sigvarth@stud.ntnu.no>
2016-05-03 23:38:53 +0200

    Added log data from lab

---
+++ MATLAB/helicopter4/LQR_bothconstraint_N=100_LQR[5,1,1,1,1,1]/constr4.m
@@ -0,0 +1,15 @@
+function [ c, ceq ] = constr4( x )
+
+    N = 100;
+    mx = 6;
+    alpha = 0.2;
+    beta = 20;
+    lambda_t=2*pi/3;
+    
+    lambda_k = x(1:6:N*mx);
+    e_k = x(5:6:N*mx);
+    
+    c = alpha*exp(-beta*((lambda_k-lambda_t).^2))-e_k;
+    ceq = [];
+end
+
---
+++ MATLAB/helicopter4/LQR_bothconstraint_N=100_LQR[5,1,1,1,1,1]/problem4_lqr.m
@@ -0,0 +1,11 @@
+%% Optimal control of Pitch/Travel with Feedback(LQ)
+
+%problem4;
+
+%%
+
+LQR.Q = diag([5, 1, 1, 1, 1, 1]);
+LQR.R = diag([1 1]);
+
+K = dlqr(A1, B1, LQR.Q, LQR.R);
+
